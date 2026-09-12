fc64d2da9a578f9500ecef1036462f935a50c24a
SamratROX <samratdas24725@gmail.com>
2026-09-12 17:26:13 +0530

    Refactor logical thinking questions in JS Q71-80.js

--- JS Q71-80.js
+++ JS Q71-80.js
@@ -1,30 +1,30 @@
 // Logical Thinking Questions
-// 1. Take two numbers and print which one is greater.
+// 71. Take two numbers and print which one is greater.
 let a = 10
 let b = 21
 let greater = Math.max(a, b)
 console.log(greater)
 
-// 2. Check whether a number lies between 10 and 50.
+// 72. Check whether a number lies between 10 and 50.
 let val = 180
 if (val > 10 && val < 50){
     console.log(`${val} is lies between 10 and 50`)
 } else console.log(`${val} is not lies between 10 and 50`)
 
-// 3. Check whether a password length is greater than 8.
+// 73. Check whether a password length is greater than 8.
 let password = 1234567
 if(String(password).length <= 8){
     console.log("Password must be greater than 8 characters")
 } else console.log("Strong Password")
 
-// 4. Check if a person can drive: age > 18 & has license = true
+// 74. Check if a person can drive: age > 18 & has license = true
 let age = 17
 let hasLicense = false
 if (age >= 18 && hasLicense === true){
     console.log("The person can Drive")
 } else console.log("Can't Drive")
 
-// 5. Check whether a number is divisible by 2, 3, or both.
+// 75. Check whether a number is divisible by 2, 3, or both.
 let num = 18
 if (num % 2 === 0 && num % 3 === 0){
     console.log(`${num} is divisivle by both 2 & 3`)
@@ -34,7 +34,7 @@ if (num % 2 === 0 && num % 3 === 0){
     console.log(`${num} is divisible by 2`)
 } else console.log(`${num} is NOT divisible by 2 or 3.`)
 
-// 6. Print `"Good Morning"`, `"Good Afternoon"`, or `"Good Evening"` based on time.
+// 76. Print `"Good Morning"`, `"Good Afternoon"`, or `"Good Evening"` based on time.
 let time = 18
 if(time >= 5 && time < 12){
     console.log("Good Morning")
@@ -42,25 +42,25 @@ if(time >= 5 && time < 12){
     console.log("Good Afternoon")
 } else console.log("Good Afternoon")
 
-// 7. Find whether a number is a multiple of 10.
+// 77. Find whether a number is a multiple of 10.
 let i = 100
 if (i % 10 === 0){
     console.log(`${i} is divisible by 10.`)
 } else console.log(`${i} is not divisible by 10.`)
 
-// 8. Create a simple discount calculator.
+// 78. Create a simple discount calculator.
 let amount = 1280
 let discount = 10
 const netAmount = amount - (amount * discount) / 100
 console.log("Net Amount =",netAmount)
 
-// 9. Check whether a product is in stock.
+// 79. Check whether a product is in stock.
 inStock = true
 if(inStock){
     console.log("Product is in stock")
 } else console.log("Product is out of stock")
 
-// 10. Calculate final bill after GST.
+// 80. Calculate final bill after GST.
 const gst = 18
 const billAfterGST = amount + (amount * discount) / 100
 console.log("Final bill after GST =", billAfterGST)
